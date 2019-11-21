cc64675a49c2c88db0bc200bdd9a8813eae67ae3
vieirawilliam <willian_43@hotmail.com>
2019-11-21 11:02:18 -0300

    Criamos pageadmin e tpl admin

--- index.php
+++ index.php
@@ -4,14 +4,25 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+//ROTA DA PAGINA PRINCIPAL
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+//ROTA DA PAGINA DE ADMINISTRAÇÃO
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
