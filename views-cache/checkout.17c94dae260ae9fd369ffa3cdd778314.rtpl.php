7f44f43c4a57124398cb8495d139a7613dee0857
vieirawilliam <willian_43@hotmail.com>
2020-01-09 14:57:17 -0300

    Site Web Service de CEP

--- views-cache/checkout.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
+++ views-cache/checkout.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -71,33 +71,36 @@
 													</tr>
 												</thead>
 												<tbody>
-                                                    
+                                                    <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
+
 													<tr class="cart_item">
 														<td class="product-name">
-															Ship Your Idea <strong class="product-quantity">× 1</strong> 
+															<?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?> <strong class="product-quantity">× <?php echo htmlspecialchars( $value1["nrqtd"], ENT_COMPAT, 'UTF-8', FALSE ); ?></strong> 
 														</td>
 														<td class="product-total">
-															<span class="amount">$700.00</span>
+															<span class="amount">R$<?php echo FunctionHTML::formatPrice($value1["vltotal"]); ?> </span>
 														</td>
                                                     </tr>
-                                                    
+                                                    <?php } ?>
+
 												</tbody>
 												<tfoot>
 													<tr class="cart-subtotal">
 														<th>Subtotal</th>
-														<td><span class="amount">$700.00</span>
+														<td><span class="amount">R$<?php echo FunctionHTML::formatPrice($cart["vlsubtotal"]); ?></span>
 														</td>
 													</tr>
 													<tr class="shipping">
 														<th>Frete</th>
 														<td>
-															$5.00
+															R$<?php echo FunctionHTML::formatPrice($cart["vlfreight"]); ?>
+
 															<input type="hidden" class="shipping_method" value="free_shipping" id="shipping_method_0" data-index="0" name="shipping_method[0]">
 														</td>
 													</tr>
 													<tr class="order-total">
 														<th>Total do Pedido</th>
-														<td><strong><span class="amount">$705.00</span></strong> </td>
+														<td><strong><span class="amount"></span></strong>R$<?php echo FunctionHTML::formatPrice($cart["vltotal"]); ?> </td>
 													</tr>
 												</tfoot>
 											</table>
